24cccee8dc0e43356361cc05846884e487970212
gokulsona <gokulsona19@gmail.com>
2024-10-26 18:31:04 +0530

    Register and Login Page Template Updated

--- login/forms.py
+++ login/forms.py
@@ -32,10 +32,10 @@ class LoginForm(AuthenticationForm):
     
     username = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder':'Your username',
-        'class':'w-full py-4 px-6 rounded-xl'
+        
     }))
 
     password = forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder':'Your password',
-        'class':'w-full py-4 px-6 rounded-xl'
+        
     }))
